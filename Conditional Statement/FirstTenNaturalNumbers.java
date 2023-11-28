4c28bdb6afe4b8430647af074e0e93079860cc54
zohaib02 <zohaib847847@gmail.com>
2023-11-28 01:28:34 +0000

    Display the first 10 natural numbers

---
+++ Conditional Statement/FirstTenNaturalNumbers.java
@@ -0,0 +1,9 @@
+public class FirstTenNaturalNumbers {
+    public static void main(String[] args) {
+        System.out.println ("The first 10 natural numbers are:\n");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+        System.out.println ("\n");
+    }
+}
