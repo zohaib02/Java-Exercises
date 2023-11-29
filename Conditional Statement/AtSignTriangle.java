a3b9f4856c757582c4df2d452208e9db605d3dff
zohaib02 <zohaib847847@gmail.com>
2023-11-29 01:45:09 +0000

    Generate a following @'s triangle

---
+++ Conditional Statement/AtSignTriangle.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class AtSignTriangle {
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input the number: ");
+        int n = in.nextInt();
+
+        for (int i = 0; i < n; i++){
+            for (int spc = n - i; spc > 0; spc--)
+                System.out.print(" ");
+            for (int j = 0; j <= i; j++)
+                System.out.print("@");
+            System.out.println();
+        }
+    }
+}
