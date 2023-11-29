43171b1064c0e82ca6f73e669d1c618f6cc20279
zohaib02 <zohaib847847@gmail.com>
2023-11-29 02:12:26 +0000

    Reads a floating-point number and print result

---
+++ Conditional Statement/ReadsAFloatingPointNumber.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class ReadsAFloatingPointNumber {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Input a number: ");
+        double n = in.nextDouble();
+
+        if (n > 0) {
+            if (n < 1) {
+                System.out.println("Positive small");
+            }
+            else if (n > 1000000) {
+                System.out.println("Positive large");
+            }
+            else {
+                System.out.println("Positive");
+            }
+        }
+        else if (n < 0) {
+            if (Math.abs(n) < 1) {
+                System.out.println("Negative small");
+            }
+            else if (Math.abs(n) > 1000000) {
+                System.out.println("Negative large");
+            }
+            else {
+                System.out.println("Negative");
+            }
+        }
+        else {
+            System.out.println("Zero");
+        }
+    }
+}
