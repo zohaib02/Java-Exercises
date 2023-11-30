8f4b457c1cf036938d42fa34872b02bce27729e7
zohaib02 <zohaib847847@gmail.com>
2023-11-30 01:50:39 +0000

    Hey there! Ever dreamed of creating your own ice cream masterpiece? Look no further! This Ice Cream Store App, built with Java and Swing, brings your ice cream fantasies to life. Choose your flavors (Chocolate, Strawberry, Vanilla) and toppings (Sprinkles, Marshmallows, Flakes, Caramel) with a click, and voilà! It calculates the total price in a snap.
    Description:
    Welcome to the Ice Cream Store! Picture a fun and interactive app that lets you craft your dream ice cream. We've put our hearts into this creation to ensure it's not just an app but an experience.
    
    Easy, Fun, and Visual:
    
    We've made it super easy to pick your flavors and toppings—just click away! The interface is designed to be intuitive and visually appealing, ensuring a seamless and delightful experience.
    Magic Happens in Real-Time:
    
    Ever seen prices change magically as you select? Well, that's us! We've got real-time calculations going on. You choose, and instantly, the total price pops up, keeping you engaged and excited.
    No Stress, Just Ice Cream Bliss:
    
    Made a mistake or want to start fresh? No worries! Hit 'Clear All' for a clean slate. It's all about making your experience stress-free and enjoyable.
    Geeky Stuff Behind the Scenes:
    
    Behind this ice cream paradise, there's some serious Java and Swing magic happening. We've poured our expertise into every detail, ensuring a smooth and responsive interface.
    A Treat for Ice Cream Lovers:
    
    We didn't just build an app; we crafted an experience. It's not just about flavors and toppings; it's about translating dreams into real, user-centric software.
    
    So, are you ready to indulge in a scoop of digital ice cream paradise? Dive into our Ice Cream Store and let your imagination create the perfect treat!

---
+++ Java Ice cream Store Application/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+
+/**
+ *
+ * @author zohaiburrehman
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Icecream_Store.main(args);
+    }
+}
