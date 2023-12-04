8c724bd4201d62caa14143694423eedb34b9c290
zohaib02 <zohaib847847@gmail.com>
2023-12-04 18:12:23 +0000

    File reading and empty file exception handling

---
+++ Exception Handling/EmptyFileCheck.java
@@ -0,0 +1,35 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class EmptyFileCheck {
+    public static void main(String[] args) {
+        try {
+            checkFileNotEmpty("test1.txt");
+            System.out.println("File is not empty.");
+        } catch (FileNotFoundException e) {
+            System.out.println("Error: " + e.getMessage());
+        } catch (EmptyFileException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    public static void checkFileNotEmpty(String fileName) throws FileNotFoundException, EmptyFileException {
+        File file = new File(fileName);
+        Scanner scanner = new Scanner(file);
+
+        if (!scanner.hasNextLine()) {
+            scanner.close();
+            throw new EmptyFileException("File is empty.");
+        }
+
+        scanner.close();
+    }
+}
+
+
+class EmptyFileException extends Exception {
+    public EmptyFileException(String message) {
+        super(message);
+    }
+}
