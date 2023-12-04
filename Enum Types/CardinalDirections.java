e66afe144d03d8765741053524c6470d40228857
zohaib02 <zohaib847847@gmail.com>
2023-12-04 17:30:29 +0000

    Write a Java program to implement an enum called "Direction" with constants representing the cardinal directions (North, South, East, West).

---
+++ Enum Types/CardinalDirections.java
@@ -0,0 +1,16 @@
+public class CardinalDirections {
+    public enum Direction {
+        NORTH,
+        SOUTH,
+        EAST,
+        WEST
+    }
+
+    public static void main(String[] args) {
+        Direction direction1 = Direction.NORTH;
+        Direction direction2 = Direction.WEST;
+
+        System.out.println("First direction: " + direction1);
+        System.out.println("Second direction: " + direction2);
+    }
+}
